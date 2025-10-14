0d2012cf48bb697380ef4bfd0f6c7efd52d4cf44
samiksha tiwari <samiksha.devops@gmail.com>
2025-10-14 13:33:28 +0530

    added varable.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "azurerm" {
     features {
       
     }
-    subscription_id = "dc1b0d18-b7a7-4981-90c1-5f01ea6fc258"
+    subscription_id = var.subscription_id
   # Configuration options
 }
 
